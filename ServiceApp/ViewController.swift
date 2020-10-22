3b6801c923be1fb55b6bbd2a55af2afc34862e48
Viktor Richard Ban <viktorrichardban@gmail.com>
2020-10-22 21:32:34 +0200

    Initial Commit

---
+++ ServiceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ServiceApp
+//
+//  Created by Viktor Bán on 2020. 10. 22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
